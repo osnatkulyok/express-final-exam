57483aa7b098f7f8a32bd373db7546f2df3e0f95
osnatkulyok <117373663+osnatkulyok@users.noreply.github.com>
2023-01-29 13:14:18 +0200

    I've tried my best

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
-import express from 'express';
+import express from 'express'
+import { Request, Response } from 'express'
+import bmi from './bmi'
 
-const app = express();
+const app = express()
 
 // TODO implement me :)
+const port = 3001
 
+app.get('/bmi', (req: Request, res: Response) => {
+  const wieght = parseFloat(req.params.wieght)
+  const weightUnit = parseFloat(req.params.weightUnit)
+  const height = parseFloat(req.params.height)
+  const heightUnit = parseFloat(req.params.heightUnit)
 
-export default app;
+  res.send(bmi())
+})
+app.post('/bmi', (req: Request, res: Response) => {
+  const wieght = parseFloat(req.body.wieght)
+  const weightUnit = parseFloat(req.body.weightUnit)
+  const height = parseFloat(req.body.height)
+  const heightUnit = parseFloat(req.body.heightUnit)
+  res.send(bmi())
+})
+
+export default app
--- src/bmi.ts
+++ src/bmi.ts
@@ -3,41 +3,104 @@
  */
 
 export const BmiStatus = {
-    Underweight: 0,
-    HealthyWeight: 1,
-    Overweight: 2,
-    Obesity: 3
+  Underweight: 0,
+  HealthyWeight: 1,
+  Overweight: 2,
+  Obesity: 3,
 }
 
-export const WeightUnits = {
-    KG: 0,     // kilograms
-    G: 1,      // grams
-    Pound: 2,  // lbs
+export let WeightUnits = {
+  KG: 0, // kilograms
+  G: 1, // grams
+  Pound: 2, // lbs
 }
 
 export const HeightUnits = {
-    CM: 0,     // centimeter
-    M: 1,      // meter
-    In: 2,     // inch
-    Ft: 3,     // feet
+  CM: 0, // centimeter
+  M: 1, // meter
+  In: 2, // inch
+  Ft: 3, // feet
 }
 
 export interface BmiInput {
-    weight: number;
-    weightUnit: number;
-    height: number;
-    heightUnit: number;
+  weight: number
+  weightUnit: number
+  height: number
+  heightUnit: number
 }
 
 export interface BmiOutput {
-    bmi: number;
-    status: number;
+  bmi: number
+  status: number
 }
 
 /**
  * Takes a BmiInput object and return BmiOutput response
  */
+// convert all weight to KG and height to CM
+ function convertInput(input: BmiInput) {
+    let weight: Number = 0
+    let height: Number = 0
+
+    if (WeightUnits.G) {
+         weight = WeightUnits.G/1000
+    }
+    if (WeightUnits.Pound) {
+         weight = WeightUnits.Pound* 0.45359237
+    }
+    if (HeightUnits.Ft) {
+         height = HeightUnits.Ft/30.48
+    }
+    if (HeightUnits.In) {
+         height = HeightUnits.In/2.54
+    }
+    if (HeightUnits.M) {
+         height = HeightUnits.M*100
+    }
+    let bmi: Number = weight/height*height
+    return bmi
+}
+
+
+async function fetchStatusBMI(params:type) {
+    const response = await fetch('http://localhost:3001/bmi', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }})
+    return await response.json()
+}
+async function fetchStatusBMI(params:type) {
+    const response = await fetch('http://localhost:3001/bmi', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }})
+    return await response.json()
+}
+
+
+ 
 export default function bmi(input: BmiInput): BmiOutput {
-    // TODO: implement me
-    return {status: 0, bmi: 0};
+    convertInput(input: BmiInput)
+    fetchStatusBMI()
+
+
+
+    let status = 0
+    let bmi = 0
+  // TODO: implement me
+  if (bmi < 18.5) {
+    status = 0
+}
+  if (bmi >= 18.5 && bmi < 25) {
+    status = 1
+}
+  if (bmi >= 25 && bmi < 30) {
+    status = 2
+}
+  if (bmi > 30) {
+    status = 3
+}
+  return {`status: ${status}, bmi: ${bmi}` }
 }
